18c0e225c0a453df44dee877fa7531537dfd69e3
tomas.payer <tomaspayer18@gmail.com>
2022-06-08 14:19:08 -0300

    Second Challenge

--- js/firstchair.js
+++ js/firstchair.js
@@ -9,4 +9,17 @@ function arrayCleaner (arr, value) {
   }
    
  console.log(arrayCleaner([1, 2, 3, 1, 2, 3], 2, 3));
+
+ // Second Challenge//
+
+ function spinalCaseConverter(str) {
+     let camelCaseHandled = str.replace(/([a-z]) ([A-Z])/g, "$1 $2");
+     let spacesAndDashedHAndled = camelCaseHandled.replace(/\s|_/g, "-")
+    return spacesAndDashedHAndled.toLowerCase();
+  }
+
+  let result = spinalCaseConverter('CONVERT-This-text-To-SpInalCase');
+  
+  console.log(result);
+  
  
