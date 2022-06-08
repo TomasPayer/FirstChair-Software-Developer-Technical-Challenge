b95233837d6f0cf90a16a314ca7880ed1c1c9221
tomas.payer <tomaspayer18@gmail.com>
2022-06-08 13:40:53 -0300

    firstchair

---
+++ js/firstchair.js
@@ -0,0 +1,11 @@
+function arrayCleaner (arr, value) {
+
+    var args = Array.prototype.slice.call(arguments);
+    args.shift();
+    return arr.filter(function(element) {
+      return args.indexOf(element) === -1;
+    });
+  }
+   
+ console.log(arrayCleaner([1, 2, 3, 1, 2, 3], 2, 3));
+ 
